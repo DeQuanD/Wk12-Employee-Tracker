b9fd8cbdb9e582a1baf02506e484a7863eab92a0
DeQuan Dickson <dequan07@gmail.com>
2022-10-29 05:48:42 -0400

    created config and schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30), 
+    salary DECIMAL NOT NULL,
+    departmentId INT,
+    FOREIGN KEY (departmentId) REFERENCES department(id) 
+);
+
+ CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    roleId INT,
+    managerId INT,
+    FOREIGN KEY (roleId) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (managerId) REFERENCES employee(id) ON DELETE SET NULL
+      );
